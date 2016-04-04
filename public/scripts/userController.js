233cdb4c6c25bb0add7530fbdcbef6bc16b3a731
joshuaroytino <joshuaroytino@gmail.com>
2016-04-04 18:04:43 +0800

    Adding a Logout Method

--- public/scripts/userController.js
+++ public/scripts/userController.js
@@ -6,7 +6,7 @@
         .module('authApp')
         .controller('UserController', UserController);
 
-    function UserController($http) {
+    function UserController($http, $auth, $state, $rootScope) {
 
         var vm = this;
 
@@ -21,6 +21,26 @@
                 vm.users = users;
             }).error(function(error) {
                 vm.error = error;
+            });
+        }
+
+        // We would normally put the logout method in the sam
+        // spot as the login method, ideally extracted out into
+        // a service. For this simpler example we'll leave it here
+        vm.logout = function(){
+            $auth.logout().then(function() {
+
+                // Remove the authenticated user from local storage
+                localStorage.removeItem('user');
+
+                // Flip authenticated to false so that we no longer
+                // show UI elements dependant on the user being logged in
+                $rootScope.authenticated = false;
+
+                // Remove the current user info from rootscope
+                $rootScope.currentUser = null;
+
+                $state.go('/auth', {});
             });
         }
     }
